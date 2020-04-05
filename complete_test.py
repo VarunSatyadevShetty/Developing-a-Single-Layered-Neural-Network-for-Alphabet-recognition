47911275f2873fe8b534fc6f6cbbc153bc7f5d16
Varun Satyadev Shetty <62276853+VarunSatyadevShetty@users.noreply.github.com>
2020-04-04 22:10:03 -0400

    Complete training, validation and testing on the noMNIST dataset

---
+++ complete_test.py
@@ -0,0 +1,304 @@
+'''import hashlib
+import os
+import pickle
+from urllib.request import urlretrieve
+
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.utils import resample
+from tqdm import tqdm
+from zipfile import ZipFile
+
+print('All modules imported.')
+###############################################
+def download(url, file):
+    """
+    Download file from <url>
+    :param url: URL to file
+    :param file: Local file path
+    """
+    if not os.path.isfile(file):
+        print('Downloading ' + file + '...')
+        urlretrieve(url, file)
+        print('Download Finished')
+
+# Download the training and test dataset.
+download('https://s3.amazonaws.com/udacity-sdc/notMNIST_train.zip', 'notMNIST_train.zip')
+download('https://s3.amazonaws.com/udacity-sdc/notMNIST_test.zip', 'notMNIST_test.zip')
+
+# Make sure the files aren't corrupted
+assert hashlib.md5(open('notMNIST_train.zip', 'rb').read()).hexdigest() == 'c8673b3f28f489e9cdf3a3d74e2ac8fa',\
+        'notMNIST_train.zip file is corrupted.  Remove the file and try again.'
+assert hashlib.md5(open('notMNIST_test.zip', 'rb').read()).hexdigest() == '5d3c7e653e63471c88df796156a9dfa9',\
+        'notMNIST_test.zip file is corrupted.  Remove the file and try again.'
+
+# Wait until you see that all files have been downloaded.
+print('All files downloaded.')
+
+##################################################
+def uncompress_features_labels(file):
+    """
+    Uncompress features and labels from a zip file
+    :param file: The zip file to extract the data from
+    """
+    features = []
+    labels = []
+
+    with ZipFile(file) as zipf:
+        # Progress Bar
+        filenames_pbar = tqdm(zipf.namelist(), unit='files')
+
+        # Get features and labels from all files
+        for filename in filenames_pbar:
+            # Check if the file is a directory
+            if not filename.endswith('/'):
+                with zipf.open(filename) as image_file:
+                    image = Image.open(image_file)
+                    image.load()
+                    # Load image data as 1 dimensional array
+                    # We're using float32 to save on memory space
+                    feature = np.array(image, dtype=np.float32).flatten()
+
+                # Get the the letter from the filename.  This is the letter of the image.
+                label = os.path.split(filename)[1][0]
+
+                features.append(feature)
+                labels.append(label)
+    return np.array(features), np.array(labels)
+
+# Get the features and labels from the zip files
+train_features, train_labels = uncompress_features_labels('notMNIST_train.zip')
+test_features, test_labels = uncompress_features_labels('notMNIST_test.zip')
+
+# Limit the amount of data to work with a docker container
+docker_size_limit = 150000
+train_features, train_labels = resample(train_features, train_labels, n_samples=docker_size_limit)
+
+# Set flags for feature engineering.  This will prevent you from skipping an important step.
+is_features_normal = False
+is_labels_encod = False
+
+# Wait until you see that all features and labels have been uncompressed.
+print('All features and labels uncompressed.')
+###########################################################################################
+# Problem 1 - Implement Min-Max scaling for grayscale image data
+def normalize_grayscale(image_data):
+    #temp_data = 0.1 + ((image_data-0)*0.8)/(255)
+    return  0.1 + ((image_data-0)*0.8)/(255)
+    """
+    Normalize the image data with Min-Max scaling to a range of [0.1, 0.9]
+    :param image_data: The image data to be normalized
+    :return: Normalized image data
+    """
+    # TODO: Implement Min-Max scaling for grayscale image data
+
+
+### DON'T MODIFY ANYTHING BELOW ###
+# Test Cases
+np.testing.assert_array_almost_equal(
+    normalize_grayscale(np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 255])),
+    [0.1, 0.103137254902, 0.106274509804, 0.109411764706, 0.112549019608, 0.11568627451, 0.118823529412, 0.121960784314,
+     0.125098039216, 0.128235294118, 0.13137254902, 0.9],
+    decimal=3)
+np.testing.assert_array_almost_equal(
+    normalize_grayscale(np.array([0, 1, 10, 20, 30, 40, 233, 244, 254,255])),
+    [0.1, 0.103137254902, 0.13137254902, 0.162745098039, 0.194117647059, 0.225490196078, 0.830980392157, 0.865490196078,
+     0.896862745098, 0.9])
+
+if not is_features_normal:
+    train_features = normalize_grayscale(train_features)
+    test_features = normalize_grayscale(test_features)
+    is_features_normal = True
+
+print('Tests Passed!')
+#########################################################
+if not is_labels_encod:
+    # Turn labels into numbers and apply One-Hot Encoding
+    encoder = LabelBinarizer()
+    encoder.fit(train_labels)
+    train_labels = encoder.transform(train_labels)
+    test_labels = encoder.transform(test_labels)
+
+    # Change to float32, so it can be multiplied against the features in TensorFlow, which are float32
+    train_labels = train_labels.astype(np.float32)
+    test_labels = test_labels.astype(np.float32)
+    is_labels_encod = True
+
+print('Labels One-Hot Encoded')
+##########################################################
+assert is_features_normal, 'You skipped the step to normalize the features'
+assert is_labels_encod, 'You skipped the step to One-Hot Encode the labels'
+
+# Get randomized datasets for training and validation
+train_features, valid_features, train_labels, valid_labels = train_test_split(
+    train_features,
+    train_labels,
+    test_size=0.05,
+    random_state=832289)
+
+print('Training features and labels randomized and split.')
+##################################################################
+# Save the data for easy access
+pickle_file = 'notMNIST.pickle'
+if not os.path.isfile(pickle_file):
+    print('Saving data to pickle file...')
+    try:
+        with open('notMNIST.pickle', 'wb') as pfile:
+            pickle.dump(
+                {
+                    'train_dataset': train_features,
+                    'train_labels': train_labels,
+                    'valid_dataset': valid_features,
+                    'valid_labels': valid_labels,
+                    'test_dataset': test_features,
+                    'test_labels': test_labels,
+                },
+                pfile, pickle.HIGHEST_PROTOCOL)
+    except Exception as e:
+        print('Unable to save data to', pickle_file, ':', e)
+        raise
+
+print('Data cached in pickle file.')'''
+#######################################################################s
+#%matplotlib inline
+
+# Load the modules
+import pickle
+import math
+
+import numpy as np
+#import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+# Reload the data
+pickle_file = 'notMNIST.pickle'
+with open(pickle_file, 'rb') as f:
+  pickle_data = pickle.load(f)
+  train_features = pickle_data['train_dataset']
+  train_labels = pickle_data['train_labels']
+  valid_features = pickle_data['valid_dataset']
+  valid_labels = pickle_data['valid_labels']
+  test_features = pickle_data['test_dataset']
+  test_labels = pickle_data['test_labels']
+  del pickle_data  # Free up memory
+
+
+print('Data and modules loaded.')
+#####################################################################
+#below is the code from training_and_testing.py
+
+n_input = 784  # MNIST data input (img shape: 28*28)
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+# Import MNIST data
+mnist = input_data.read_data_sets('/datasets/ud730/mnist', one_hot=True)
+
+# The features are already scaled and the data is shuffled
+#in simple words thry are the X(inputs)
+train_features = mnist.train.images
+test_features = mnist.test.images
+#in simple words they are y(the correct label)
+train_labels = mnist.train.labels.astype(np.float32)
+test_labels = mnist.test.labels.astype(np.float32)
+
+# Weights & bias
+weights = tf.Variable(tf.random_normal([n_input, n_classes]))
+bias = tf.Variable(tf.zeros([n_classes]))
+
+#defining the features
+features = tf.placeholder(tf.float32,[None,n_input])
+labels = tf.placeholder(tf.float32,[None,n_classes])
+#use of None is that we cam implement a batch_size in its place during session
+
+training_feed_dict = {features: train_features, labels: train_labels}
+validation_feed_dict = {features: valid_features, labels: valid_labels}
+testing_feed_dict = {features: test_features, labels: test_labels}
+
+#doing yhat = xw + b
+logits = tf.add(tf.matmul(features,weights),bias)
+
+#calculating the error using cross entropy
+#E = -1/m sumof(y*ln(p))
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+
+#the alternative to above is
+'''probability = tf.nn.softmax(logits)
+cost = -tf.reduce_sum(labels* tf.log(probability),axis=1)'''
+
+#calculating the loss
+loss = tf.reduce_mean(cost)
+
+#initializing the variables
+init = tf.global_variables_initializer()
+
+#setting the the parameters
+epoch = 5
+batch_size = 100
+learning_rate = 0.2
+
+#updating the weights using the Gradient Descent method
+#wi = wi - (learing_rate*(dE/dWi))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+
+def batches(batch_size, features, labels):
+    """
+    Create batches of features and labels
+    :param batch_size: The batch size
+    :param features: List of features
+    :param labels: List of labels
+    :return: Batches of (Features, Labels)
+    """
+    assert len(features) == len(labels)
+    outout_batches = []
+
+    sample_size = len(features)
+    for start_i in range(0, sample_size, batch_size):
+        end_i = start_i + batch_size
+        batch = [features[start_i:end_i], labels[start_i:end_i]]
+        outout_batches.append(batch)
+
+    return outout_batches
+
+with tf.Session() as sess:
+    sess.run(init)
+    sess.run(loss,feed_dict=training_feed_dict)
+    sess.run(loss,feed_dict=validation_feed_dict)
+    sess.run(loss,feed_dict=testing_feed_dict)
+    '''Calculate accuracy for test dataset
+    test_accuracy = sess.run(
+    accuracy,
+    feed_dict={features: test_features, labels: test_labels})''' # this is equivalentsess.run(accuracy, feed_dict=testing_feed_dict)
+
+#calculating the accuracy
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+with tf.Session() as sess:
+    sess.run(init)
+    for epoch in range(epoch):
+        for batch_features, batch_labels in batches(batch_size, train_features, train_labels):
+            #sess.run(optimizer, feed_dict={features: batch_features, labels: batch_labels})
+            _, l = sess.run(
+                [optimizer, loss],
+            feed_dict={features: batch_features, labels: batch_labels})
+            accuracy_training = sess.run(accuracy, feed_dict=training_feed_dict)
+            #print("coming up is the training accuracy")
+            #print(accuracy_training)
+            accuracy_validation = sess.run(accuracy, feed_dict=validation_feed_dict)
+            #print("coming up is the validation accuracy")
+            #print(accuracy_validation)
+            accuracy_testing = sess.run(accuracy, feed_dict=testing_feed_dict)
+            #print("coming up is the testing accuracy")
+            #print(accuracy_testing)
+    print("coming up is the training accuracy")
+    print(accuracy_training)
+    print("coming up is the validation accuracy")
+    print(accuracy_validation)
+    print("coming up is the testing accuracy")
+    print(accuracy_testing)
